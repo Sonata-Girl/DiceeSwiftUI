0cf19dc606ad39bcba399ec8f6d9d575c095da99
SonataGirl <mike@magic.org>
2023-06-04 23:17:51 +0300

    Initial Commit

---
+++ DiceeSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiceeSwiftUI
+//
+//  Created by Sonata Girl on 04.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Sonata Girl on 04.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
